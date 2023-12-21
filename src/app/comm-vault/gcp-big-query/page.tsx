b32733fe0ceb3f819a7833b1b783953ded394db8
Marco Salazar <marco@elementl.com>
2023-12-21 15:24:14 -0500

    page scaffolding

---
+++ src/app/comm-vault/gcp-big-query/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>GCP Big Query</div>;
+};
+
+export default Page;
---
+++ src/app/comm-vault/prd-xbsa/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+  return <div>Prd xbsa</div>;
+};
+
+export default Page;
